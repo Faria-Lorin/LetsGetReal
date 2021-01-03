c6f85910aeb7245972d9b53522a4814c64d27356
Faria-Lorin <florin20@stuy.edu>
2021-01-03 17:53:41 -0500

    I took out the 0.0 that was inputed into super in the constructor, because no arguments are required.

--- RationalNumber.java
+++ RationalNumber.java
@@ -9,7 +9,7 @@ public class RationalNumber extends Number
   *@param deno the denominator
   */
   public RationalNumber(int nume, int deno){
-    super(0.0);//this value is ignored!
+    super();
     if (deno == 0 || nume == 0){
       deno = 1;
       nume = 0;
